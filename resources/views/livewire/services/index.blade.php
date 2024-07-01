7c549aab807270906c0d116afd11f54af1703fbe
Andre <andrwijaya988@gmail.com>
2024-07-01 21:35:11 +0700

    feat: add wire:navigate to link

--- resources/views/livewire/services/index.blade.php
+++ resources/views/livewire/services/index.blade.php
@@ -39,7 +39,7 @@
                                 {{ \Carbon\Carbon::parse($service->start_at)->format('H:i') }} -
                                 {{ \Carbon\Carbon::parse($service->end_at)->format('H:i') }} </p>
                         </div>
-                        <a href="/services/{{ $service->id }}"
+                        <a href="/services/{{ $service->id }}" wire:navigate
                             class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             Read more
                             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
