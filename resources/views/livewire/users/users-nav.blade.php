3327436cdf1588f67166430db31ab1856c2b96f7
Andre <andrwijaya988@gmail.com>
2024-07-05 22:34:19 +0700

    feat: add user ui model create

--- resources/views/livewire/users/users-nav.blade.php
+++ resources/views/livewire/users/users-nav.blade.php
@@ -24,13 +24,13 @@
                         class="block py-2 px-3 text-white bg-blue-700 rounded dark:text-white md:dark:text-blue-500">
                         Tambah User</button>
 
-                    <x-modal id="modal1" title="Add Service">
+                    <x-modal id="modal1" title="Add User">
                         <x-slot:body>
                             <form class="p-4 md:p-5" wire:submit="createNewService">
                                 <div class="grid gap-4 mb-4 grid-cols-2 ">
                                     <div class="col-span-2">
                                         <label for="name"
-                                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Event
+                                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                                             Name</label>
                                         <input type="text" id="name" wire:model="name"
                                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
@@ -38,65 +38,17 @@
                                     </div>
                                     <div class="col-span-2">
                                         <label for="date"
-                                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
+                                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                                         <div class="relative max-w-sm">
                                             <div
                                                 class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-                                                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                    viewBox="0 0 20 20">
-                                                    <path
-                                                        d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-                                                </svg>
                                             </div>
-                                            <input datepicker datepicker-autohide datepicker-autoselect-today
-                                                @change-date.camel="@this.set('date', $event.target.value)"
-                                                datepicker-format="dd/mm/yyyy" type="text" id="date"
-                                                wire:model="date" datepicker-orientation="botton"
-                                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                                placeholder="Select date">
+                                            <input type="password" id="date" wire:model="date"
+                                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                placeholder="***********">
                                         </div>
+                                    </div>
 
-
-                                    </div>
-                                    <div class="col-span-2 sm:col-span-1">
-                                        <label for="start"
-                                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Start</label>
-                                        <div class="relative">
-                                            <div
-                                                class="absolute inset-y-0 end-0 top-0 flex items-center pe-3.5 pointer-events-none">
-                                                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                    viewBox="0 0 24 24">
-                                                    <path fill-rule="evenodd"
-                                                        d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z"
-                                                        clip-rule="evenodd" />
-                                                </svg>
-                                            </div>
-                                            <input type="time" id="start" wire:model="start_at"
-                                                class="bg-gray-50 border leading-none border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                                required />
-                                        </div>
-                                    </div>
-                                    <div class="col-span-2 sm:col-span-1">
-                                        <label for="end"
-                                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">End</label>
-                                        <div class="relative">
-                                            <div
-                                                class="absolute inset-y-0 end-0 top-0 flex items-center pe-3.5 pointer-events-none">
-                                                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                    viewBox="0 0 24 24">
-                                                    <path fill-rule="evenodd"
-                                                        d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z"
-                                                        clip-rule="evenodd" />
-                                                </svg>
-                                            </div>
-                                            <input type="time" id="end" wire:model="end_at"
-                                                class="bg-gray-50 border leading-none border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                                required />
-                                        </div>
-                                    </div>
 
                                 </div>
                                 <button type="submit"
