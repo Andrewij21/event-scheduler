77e38b835550548b64d16e88bb8cd5ce15e0a432
Andre <andrwijaya988@gmail.com>
2024-07-13 09:50:08 +0700

    feat: add all option

--- resources/views/livewire/services/detail/service-detail-nav.blade.php
+++ resources/views/livewire/services/detail/service-detail-nav.blade.php
@@ -31,10 +31,14 @@
                 <div id="dropdown"
                     class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
                     <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+                        <li>
+                            <button wire:click="$set('searchDivision', '')"
+                                class="block w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Semua</button>
+                        </li>
                         @foreach ($this->divisions as $division)
                             <li wire:key="{{ $division->id }}">
                                 <button wire:click="$set('searchDivision', '{{ $division->id }}')"
-                                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ $division->name }}</button>
+                                    class="block w-full text-left px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ $division->name }}</button>
                             </li>
                         @endforeach
                     </ul>
